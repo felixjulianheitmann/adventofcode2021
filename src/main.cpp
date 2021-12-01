3292ce1f35d9c93d8d3255d728ca77d9b777bef3
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-01 12:39:31 +0100

    Icluded cli argument reader in main template

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
+
+std::vector<std::string> readArgs(int argc, char const *argv[]);
 
 int main(int argc, char const *argv[])
 {
     // This is a template main
     return 0;
+}
+
+std::vector<std::string> readArgs(int argc, char const *argv[])
+{
+    std::vector<std::string> args;
+    for(int i = 1; i < argc; i++)
+        args.push_back(argv[i]);
+
+    return args;
 }
