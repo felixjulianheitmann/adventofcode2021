c216c329b0d23a4f67af128b6e3e47bdaf223b0d
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-01 12:38:06 +0100

    Implemented solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+std::vector<std::string> readArgs(int, char const *[]);
 
 int main(int argc, char const *argv[])
 {
-    // This is a template main
-    return 0;
+    std::vector<std::string> args = readArgs(argc, argv);
+    if(args.size() == 0) return EXIT_FAILURE;
+
+    std::ifstream is;
+    is.open(args.at(0));
+
+    int current = 0, previous = 0;
+    int increaseCounter = 0;
+    if(!is.eof()) is >> previous;
+
+    while (!is.eof())
+    {
+        is >> current;
+        if(current > previous) increaseCounter++;
+    }
+
+    std::cout << "There are " << increaseCounter << " measurements exceeding its predecessor." << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
+std::vector<std::string> readArgs(int argc, char const *argv[])
+{
+    std::vector<std::string> args;
+    for(int i = 1; i < argc; i++)
+        args.push_back(argv[i]);
+
+    return args;
 }
