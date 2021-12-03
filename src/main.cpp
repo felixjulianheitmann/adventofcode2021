0a876e06e3ad1caf9500f62be8718b677852381e
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-03 10:12:06 +0100

    Updated template main to read the arguments and open a file stream

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 std::vector<std::string> readArgs(int argc, char const *argv[]);
 
 int main(int argc, char const *argv[])
 {
-    // This is a template main
-    return 0;
+    auto args = readArgs(argc, argv);
+    if(args.empty()) return EXIT_FAILURE;
+
+    std::ifstream is(args[0]);
+
+    // template main
+
+    return EXIT_SUCCESS;
 }
 
 std::vector<std::string> readArgs(int argc, char const *argv[])
