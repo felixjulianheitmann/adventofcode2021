5d209c0eaa36d6cac99d038da5621c36a686f54c
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-03 10:46:40 +0100

    Implemented a solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
+#include <array>
 
 std::vector<std::string> readArgs(int argc, char const *argv[]);
+
+constexpr int nBits = 12;
 
 int main(int argc, char const *argv[])
 {
@@ -10,8 +14,29 @@ int main(int argc, char const *argv[])
     if(args.empty()) return EXIT_FAILURE;
 
     std::ifstream is(args[0]);
+    std::string tmp;
+    std::array<int, nBits> amount{ 0 };
+    int i;
 
-    // template main
+    for (i = 0; !is.eof(); i++)
+    {
+        is >> tmp;
+        std::bitset<nBits> bits(tmp);
+        for(int j = 0; j < nBits; j++)
+            amount[j] += bits[j];
+    }
+
+    unsigned gamma = 0;
+    for(int j = 0; j < nBits; j++)
+    {
+        if(amount[j] > (i/2))
+            gamma |=  1 << j;
+    }
+    unsigned epsilon = gamma ^ ( (2 << nBits-1) - 1 );
+
+    std::cout << "The gamma rate is " << std::bitset<nBits>(gamma).to_string() << " (" << std::dec << gamma << ")" << std::endl;
+    std::cout << "The epsilon rate is " << std::bitset<nBits>(epsilon).to_string() << " (" << std::dec << epsilon << ")" << std::endl;
+    std::cout << "Their product is " << gamma * epsilon << std::endl;
 
     return EXIT_SUCCESS;
 }
