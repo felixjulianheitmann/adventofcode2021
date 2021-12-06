2547d6aee2b3cb3fd5e012574f6ed47621ccc6a1
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-06 11:35:16 +0100

    Implemented a solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <numeric>
 
 std::vector<std::string> readArgs(int argc, char const *argv[]);
 std::vector<std::string> splitString(std::string src, std::string delim);
+
+constexpr int Days = 80;
+class LanternFish
+{
+public:
+    LanternFish(int startCounter = 8) : _counter(startCounter) {};
+    bool decrCounter()
+    {
+        if(--_counter < 0) {
+            _counter = 6;
+            return true;
+        }
+        return false;
+    }
+    int getCounter() const { return _counter; }
+    operator int() const { return _counter; }
+private:
+    int _counter;
+};
+
 
 int main(int argc, char const *argv[])
 {
     auto args = readArgs(argc, argv);
     if(args.empty()) return EXIT_FAILURE;
 
+    // Read input
     std::ifstream is(args[0]);
+    std::string tmp;
+    is >> tmp;
+    auto inputs = splitString(tmp, ",");
+    std::vector<LanternFish> population;
+    for(auto const v : inputs) population.push_back(std::stoi(v));
 
-    // template main
+    // Start simulation
+    for(int i = 0; i < Days; ++i)
+    {
+        int newFish = 0;
+        for(auto &fish : population)
+        {
+            if(fish.decrCounter()) ++newFish;
+        }
+        population.resize(population.size() + newFish);
+    }
+
+    std::cout << "There are " << population.size() << " fish in the sea.";
 
     return EXIT_SUCCESS;
 }
