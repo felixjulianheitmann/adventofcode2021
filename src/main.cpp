27162d8ca1eaff9664cde9b65eb9ac3e2a8c32ab
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-06 11:13:48 +0100

    Inlcuded a splitString method in main template

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 std::vector<std::string> readArgs(int argc, char const *argv[]);
+std::vector<std::string> splitString(std::string src, std::string delim);
 
 int main(int argc, char const *argv[])
 {
@@ -23,4 +24,25 @@ std::vector<std::string> readArgs(int argc, char const *argv[])
         args.push_back(argv[i]);
 
     return args;
+}
+
+
+std::vector<std::string> splitString(std::string src, std::string delim)
+{
+    std::vector<std::string> ret;
+    while(true)
+    {
+        auto pos = src.find(delim, 0);
+        ret.push_back(src.substr(0, pos));
+        if(pos != std::string::npos)
+        {
+            src = src.substr(pos + delim.size());
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return ret;
 }
