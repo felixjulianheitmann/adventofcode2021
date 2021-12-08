25596daaca3578db378080eab17a3d28394e05e5
Felix Heitmann <felixjulianheitmann@gmail.com>
2021-12-08 14:25:20 +0100

    Implemented a solution

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 #include <vector>
 #include <type_traits>
 #include <concepts>
+#include <array>
+#include <numeric>
 
 std::vector<std::string> readArgs(int argc, char const *argv[]);
 
+using DigitDef = std::array<std::string, 10>;
+using DigitVal = std::array<std::string, 4>;
+
 template <typename T>
-requires std::floating_point<T> || std::integral<T>
+requires std::floating_point<T> || std::integral<T> || std::same_as<T, std::string>
 std::vector<T> splitString(std::string src, std::string delim);
 
 int main(int argc, char const *argv[])
@@ -15,9 +20,43 @@ int main(int argc, char const *argv[])
     auto args = readArgs(argc, argv);
     if(args.empty()) return EXIT_FAILURE;
 
+    // Read in inputs
     std::ifstream is(args[0]);
+    std::vector<std::pair<DigitDef, DigitVal>> inputs;
+    for(std::string tmp; std::getline(is, tmp); )
+    {
+        auto tmpVec = splitString<std::string>(tmp, " ");
+        inputs.push_back({});
+        std::copy(tmpVec.begin(),      tmpVec.begin()+10, inputs.back().first.begin());
+        std::copy(tmpVec.begin() + 11, tmpVec.begin()+15, inputs.back().second.begin());
+    }
 
-    // template main
+    // Compute the number of 1,4,7 and 8 in the values
+
+    int amount = std::accumulate(
+        inputs.begin(),
+        inputs.end(),
+        0,
+        [](auto const v, auto const line)
+        {
+            return v + std::accumulate(
+                line.second.begin(),
+                line.second.end(),
+                0,
+                [](auto const v, auto const digit)
+                {
+                    if(    digit.size() == 2 // digit 1
+                        || digit.size() == 4 // digit 4
+                        || digit.size() == 3 // digit 7
+                        || digit.size() == 7 // digit 8
+                        ) return v + 1;
+                    else return v;
+                }
+            );
+        }
+    );
+
+    std::cout << "The amount of 1, 4, 7, and 8 is " << amount << std::endl;
 
     return EXIT_SUCCESS;
 }
@@ -32,7 +71,7 @@ std::vector<std::string> readArgs(int argc, char const *argv[])
 }
 
 template <typename T>
-requires std::floating_point<T> || std::integral<T>
+requires std::floating_point<T> || std::integral<T> || std::same_as<T, std::string>
 std::vector<T> splitString(std::string src, std::string delim)
 {
     std::vector<T> ret;
@@ -46,6 +85,8 @@ std::vector<T> splitString(std::string src, std::string delim)
                 ret.push_back(std::stol(strVal));
             else
                 ret.push_back(std::stoul(strVal));
+        else if constexpr (std::is_same<T, std::string>())
+            ret.push_back(strVal);
         else
             ret.push_back(std::stod(strVal));
 
